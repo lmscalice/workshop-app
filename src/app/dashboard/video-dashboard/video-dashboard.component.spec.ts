53cdc7ec31b3e0dd97a8ba2b7546ada558374d92
lmscalice <lmscalice@gmail.com>
2022-09-13 14:04:49 -0400

    Adding Components and Routing

---
+++ src/app/dashboard/video-dashboard/video-dashboard.component.spec.ts
@@ -0,0 +1,23 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { VideoDashboardComponent } from './video-dashboard.component';
+
+describe('VideoDashboardComponent', () => {
+  let component: VideoDashboardComponent;
+  let fixture: ComponentFixture<VideoDashboardComponent>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      declarations: [ VideoDashboardComponent ]
+    })
+    .compileComponents();
+
+    fixture = TestBed.createComponent(VideoDashboardComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
